769b283d6527bbfefab4ff9dcc57e2d6867162ed
karen-maria-philip <karenmariaphilip@gmail.com>
2024-06-09 16:08:20 +0300

    calculator

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -5,61 +5,65 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.calculator.databinding.ActivityMainBinding
 import kotlin.math.sign
 
 class MainActivity : AppCompatActivity() {
-    lateinit var etNum1: EditText
-    lateinit var etNum2: EditText
-    lateinit var btn_add: Button
-    lateinit var btn_subtract: Button
-    lateinit var btn_divide: Button
-    lateinit var btn_multiplication: Button
-    lateinit var tvResult:TextView
+//    lateinit var etNum1: EditText
+//    lateinit var etNum2: EditText
+//    lateinit var btn_add: Button
+//    lateinit var btn_subtract: Button
+//    lateinit var btn_divide: Button
+//    lateinit var btn_multiplication: Button
+//    lateinit var tvResult:TextView
+lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        castViews()
-        btn_add.setOnClickListener {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+//        castViews()
+        binding.btnAdd.setOnClickListener {
           validate("+")
         }
-        btn_subtract.setOnClickListener {
+        binding.btnSubtract.setOnClickListener {
             validate("-")
         }
 
-        btn_multiplication.setOnClickListener {
+        binding.btnMultiplication.setOnClickListener {
             validate("*")
         }
 
 
-        btn_divide.setOnClickListener {
+        binding.btnDivision.setOnClickListener {
             validate("/")
         }
     }
 
-        fun castViews() {
-            etNum1 = findViewById(R.id.etNum1)
-            etNum2 = findViewById(R.id.etNum2)
-            btn_add = findViewById(R.id.btn_add)
-            btn_subtract = findViewById(R.id.btn_subtract)
-            btn_multiplication = findViewById(R.id.btn_multiplication)
-            btn_divide = findViewById(R.id.btn_division)
-            tvResult= findViewById(R.id. textView)
-
-
-    }
+//        fun castViews() {
+//            etNum1 = findViewById(R.id.etNum1)
+//            etNum2 = findViewById(R.id.etNum2)
+//            btn_add = findViewById(R.id.btnAdd)
+//            btn_subtract = findViewById(R.id.btnSubtract)
+//            btn_multiplication = findViewById(R.id.btnMultiplication)
+//            btn_divide = findViewById(R.id.btnDivision)
+//            tvResult= findViewById(R.id. textView)
+//
+//
+//    }
 
     fun validate(sign: String) {
-        val num1 = etNum1.text.toString()
-        val num2 = etNum1.text.toString()
+        val num1 = binding.etnum1.text.toString()
+        val num2 = binding.etnum2.text.toString()
+
         var inputError = false
         if (num1.isBlank()) {
             inputError = true
-            etNum1.error = "Num 1 is required"
+            binding.etnum1.error = "Num 1 is required"
         }
         if (num2.isBlank()) {
             inputError = true
-            etNum1.error = "Num 2 is required"
+            binding.etnum2.error = "Num 2 is required"
         }
         if(!inputError){
             calculate(num1.toDouble(), num2.toDouble(), sign)
@@ -78,6 +82,7 @@ class MainActivity : AppCompatActivity() {
             "/" -> result =  num1 / num2
 
         }
-        tvResult.text= result.toString()
+        binding.tvResult.text= result.toString()
     }
 }
+
